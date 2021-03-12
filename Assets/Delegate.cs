63d5728f52789295f5303570bf461aa4fe5644f8
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-03-12 13:03:09 +0100

    Unity Project

---
+++ Assets/Delegate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delegate : MonoBehaviour
+{
+    public delegate void Clicked();
+    public event Clicked wasClicked;
+    
+    private void OnMouseDown() {
+        wasClicked();
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int health = 100;
+    public Text healthText;
+
+    private void Awake() {
+       var Delegate = GetComponent<Delegate>();
+       Delegate.wasClicked += TakeDamage;
+    }
+    
+    void TakeDamage(){
+        int randInt = Random.Range(1, 21);
+        health -= randInt;
+        print("damage is: " + randInt);
+    }
+
+    void Update()
+    {
+        healthText.text = health.ToString();
+        if(health <= 0){
+            healthText.text = "0";
+            print("Ik ben dood");
+            Destroy(gameObject);
+        }
+    }
+}
